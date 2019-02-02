f90084788aee588c6fca82953d1b8b1eb13f58e9
ismail simsek <ismailsimsek09@gmail.com>
2019-02-02 14:38:37 +0100

    test spec update

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,29 +2,40 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from '../CommentBox';
 
-describe('CommentBox Component Test', () => {
 
-  let component;
+let component;
+
+beforeEach(() => {
+  component = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
+
+it('has a text area and a button', () => {
+  expect(component.find('textarea').length).toEqual(1);
+  expect(component.find('button').length).toEqual(1);
+});
+
+describe('the text area ', () => {
+
+  let testComment = 'test comment';
 
   beforeEach(() => {
-    component = mount(<CommentBox />);
-  });
-
-  afterEach(() => {
-    component.unmount();
-  });
-
-
-  it('has a text area and a button', () => {
-    expect(component.find('textarea').length).toEqual(1);
-    expect(component.find('button').length).toEqual(1);
+    component.find('textarea').simulate('change', { target: { value: testComment } });
+    component.update();
   });
 
   it('has a text area that users can type in', () => {
-    const testComment = 'test comment';
-    component.find('textarea').simulate('change', { target: { value: testComment } });
-    component.update();
     expect(component.find('textarea').prop('value')).toEqual(testComment);
+  });
+
+  it('form is submitted, textarea gets emptied', () => {
+    component.find('textarea').simulate('submit');
+    component.update();
+    expect(component.find('textarea').prop('value')).toEqual('');
   });
 
 });
