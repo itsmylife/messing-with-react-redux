f72a62107f3168f85537ff6b9850fe394e5fda6e
ismail simsek <ismailsimsek09@gmail.com>
2019-02-03 14:36:48 +0100

    add fetch comments button

--- src/components/CommentBox/index.js
+++ src/components/CommentBox/index.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import * as actions from 'actions'
+import * as actions from 'actions';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CommentBox extends Component {
   state = {
     comment: '',
-  }
+  };
 
-  onCommentChange = e => this.setState({ comment: e.target.value })
+  onCommentChange = e => this.setState({ comment: e.target.value });
   onFormSubmit = e => {
-    e.preventDefault()
+    e.preventDefault();
 
-    this.props.saveComment(this.state.comment)
+    this.props.saveComment(this.state.comment);
 
-    this.setState({ comment: '' })
-  }
+    this.setState({ comment: '' });
+  };
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit}>
+      <form onSubmit={ this.onFormSubmit }>
         <h4>Add a Comment</h4>
-        <textarea value={this.state.comment} onChange={this.onCommentChange} />
+        <textarea value={ this.state.comment } onChange={ this.onCommentChange } />
         <div>
           <button>
             Submit Comment
           </button>
+          <button>
+            Fetch Comments
+          </button>
         </div>
       </form>
-    )
+    );
   }
 }
 
 export default connect(
   null,
   actions,
-)(CommentBox)
+)(CommentBox);
